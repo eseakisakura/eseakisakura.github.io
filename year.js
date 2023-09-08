6a5dd8fbfa34a9fd74a846ef9784e39a4a66f063
eseakisakura <bug.jamming@aol.com>
2023-09-09 08:23:54 +0900

    2023 09 09

---
+++ year.js
@@ -0,0 +1,8 @@
+let today = new Date();
+
+let year = today.getFullYear();
+// let month = today.getMonth() + 1;
+// let day = today.getDate();
+
+document.write(year);
+// document.write(year + '年' + month + '月' + day + '日');
